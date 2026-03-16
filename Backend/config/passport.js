0873fa132b1a660fa676571a2012050dcb917484
BachhavPritesh <pritesh.v.bachhav.cg@gmail.com>
2026-03-16 13:56:31 +0530

    Add Google OAuth authentication

---
+++ Backend/config/passport.js
@@ -0,0 +1,55 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
+
+module.exports = function(passport) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL || '/api/auth/google/callback',
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          let user = await User.findOne({ googleId: profile.id });
+
+          if (user) {
+            return done(null, user);
+          }
+
+          user = await User.findOne({ email: profile.emails[0].value });
+
+          if (user) {
+            user.googleId = profile.id;
+            await user.save();
+            return done(null, user);
+          }
+
+          user = await User.create({
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            googleId: profile.id,
+            password: null,
+          });
+
+          return done(null, user);
+        } catch (error) {
+          return done(error, null);
+        }
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (error) {
+      done(error, null);
+    }
+  });
+};
